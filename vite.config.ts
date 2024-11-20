ad4b726ad83860776fcd8c3fcc746a2ec3650a77
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-20 03:21:38 -0800

    umai

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  test: {
-    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'], // Ensure it matches your file naming convention
-    exclude: [
-      'node_modules',
-      'dist',
-      'cypress',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-    ],
-  },
+	plugins: [sveltekit()]
 });
