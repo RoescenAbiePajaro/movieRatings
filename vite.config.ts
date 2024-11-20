484a1e5c849813e39cb6bcdd8eb1c2e9c1a73709
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-20 03:30:01 -0800

    Commit 9

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  build: {
+    rollupOptions: {
+      input: '**/*.{test,spec}.?(c|m)[jt]s?(x)',
+      external: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/cypress/**',
+        '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
+      ]
+    }
+  }
 });
