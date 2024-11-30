16b9d32b097fb28744e8cbb05d8105ef194f9cdb
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-30 00:45:44 -0800

    h

--- svelte.config.js
+++ svelte.config.js
@@ -15,33 +15,13 @@
 
 // anotherone
 
-// import adapter from '@sveltejs/adapter-vercel';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-//   kit: {
-//     // Other configurations
-//     adapter: adapter(),
-//   },
-// };
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
+    // Other configurations
+    adapter: adapter(),
   },
 };
 
