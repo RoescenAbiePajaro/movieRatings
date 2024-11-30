96d4cbfd5ba3635cc46139f2b2e483c38d8f5dc0
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-11-30 02:51:39 -0800

    s

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,34 @@
-import adapterStatic from '@sveltejs/adapter-static';
-import adapterVercel from '@sveltejs/adapter-vercel';
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// /** @type {import('@sveltejs/kit').Config} */
+// const config ={
+// preprocess: vitePreprocess(),
+// kit: {
+// adapter: adapter({ pages: "build", assets: "build", fallback: undefined, precompress: false, strict: true }),
+// },
+// };
+// export default config;
 
+// import adapter from '@sveltejs/adapter-vercel';
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter(),
+//   },
+// };
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: process.env.VERCEL ? adapterVercel() : adapterStatic({
+    adapter: adapter({
       pages: 'build',
       assets: 'build',
       fallback: undefined,
       precompress: false,
       strict: true
     })
-  }
+  },
 };
-
 export default config;
