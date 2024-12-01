9ea64e83adf92eaa1f4ca7d9b79986dde7409e44
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-01 04:49:36 -0800

    g

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,8 @@ import adapterVercel from '@sveltejs/adapter-vercel';
 const config = {
   kit: {
     adapter: process.env.VERCEL ? adapterVercel() : adapterStatic({
-      pages: 'public',
-      assets: 'public',
+      pages: 'build',
+      assets: 'build',
       fallback: undefined,
       precompress: false,
       strict: true
