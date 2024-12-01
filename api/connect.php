fe833925be3f6bdd368d61eb7ed69f27c0fb17cb
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-01 02:35:50 -0800

    oks

--- api/connect.php
+++ api/connect.php
@@ -1,25 +1,8 @@
-<!-- connect.php -->
 <?php
-// $servername = "localhost";
-// $username = "root"; // Default XAMPP username
-// $password = ""; // Default XAMPP password is empty
-// $dbname = "movie_reviews"; // Database name
-
-// try {
-//     // Establish PDO connection
-//     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     echo json_encode(['error' => 'Connection failed: ' . $e->getMessage()]);
-//     exit();
-// }
-?>
-
-<?php
-$servername = "sql304.infinityfree.com";
-$username = "	if0_37824771"; // Default XAMPP username
-$password = "yeboi222GB"; // Default XAMPP password is empty
-$dbname = "if0_37824771_movie_reviews"; // Database name
+$servername = "localhost";
+$username = "root"; // Default XAMPP username
+$password = ""; // Default XAMPP password is empty
+$dbname = "movie_reviews"; // Database name
 
 try {
     // Establish PDO connection
@@ -30,4 +13,3 @@ try {
     exit();
 }
 ?>
-
