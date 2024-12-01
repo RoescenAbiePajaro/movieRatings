928955d96e9b50f01282a01c354976777615f872
RoescenAbiePajaro <nanatsugg168@gmail.com>
2024-12-01 00:45:44 -0800

    f

--- api/post.php
+++ api/post.php
@@ -2,7 +2,7 @@
 
 
 // CORS headers to allow requests from your frontend (ensure frontend matches this domain)
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: https://svelte-screen-github-io.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
